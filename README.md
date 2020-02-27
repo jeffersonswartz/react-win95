d48a3b1f48fc0ebc27d4887aa1e8b93777086fa3
Jefferson Swartz <jeffersonswartz1994@gmail.com>
2020-02-27 13:10:37 +0000

    Added background and updated netlify badge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-win95
- 
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/6f916e0b-7355-4f41-98fa-78f5caabd140/deploy-status)](https://app.netlify.com/sites/react-win95/deploys) 
--- src/Components/Desktop/Desktop.tsx
+++ src/Components/Desktop/Desktop.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Desktop: React.FC<Props> = () => {
     return (
         <>
-            <div style={{display: 'flex', flex: 1}}>
+            <div style={{display: 'flex', flex: 1, background: '#008080'}}>
                 Content
             </div>
             <TaskBar />
